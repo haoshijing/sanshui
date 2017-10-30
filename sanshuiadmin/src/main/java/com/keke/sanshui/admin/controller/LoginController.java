1c8d5769a6b908a7e77ae39feb3b5f31100730d1
haoshijing <haoshijing690@163.com>
2017-10-31 00:32:03 +0800

    提交登陆代码

--- sanshuiadmin/src/main/java/com/keke/sanshui/admin/controller/LoginController.java
+++ sanshuiadmin/src/main/java/com/keke/sanshui/admin/controller/LoginController.java
@@ -1,13 +1,15 @@
 package com.keke.sanshui.admin.controller;
 
+import com.keke.sanshui.admin.request.LoginDataRequest;
 import com.keke.sanshui.base.admin.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class TestController {
+public class LoginController {
 
     @Autowired
     private AdminService adminService;
@@ -19,7 +21,7 @@ public class TestController {
 
     @RequestMapping("/login")
     @ResponseBody
-    public Boolean login(String name,String password){
-        return  adminService.checkUser(name,password);
+    public Boolean login(@RequestBody LoginDataRequest loginDataRequest){
+        return  adminService.checkUser(loginDataRequest.getName(),loginDataRequest.getPassword());
     }
 }
