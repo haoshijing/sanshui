1f8f63758032724bc3c5ca966739471483051479
郝仕兢 <haoshijing@corp.netease.com>
2017-11-13 11:04:27 +0800

    代码错误

--- sanshuiadmin/src/main/java/com/keke/sanshui/admin/request/QueryAgentPo.java
+++
@@ -1,9 +0,0 @@
-package com.keke.sanshui.admin.request;
-
-import com.keke.sanshui.base.admin.po.AgentPo;
-
-public class QueryAgentPo extends AgentPo {
-    private int limit;
-    private int pageNum;
-
-}
--- sanshuibase/src/main/java/com/keke/sanshui/base/admin/dao/PlayerPickTotalDAO.java
+++ sanshuibase/src/main/java/com/keke/sanshui/base/admin/dao/PlayerPickTotalDAO.java
@@ -23,9 +23,5 @@ public interface PlayerPickTotalDAO {
      * @param week
      * @return
      */
-<<<<<<< b840df4c9e00a12e38e502c26442d4e7ead23540
-    Long sumPickUp(@Param("playerIds") List<Integer> playerIds,@Param("week") Integer week);
-=======
     Long sumPickUp(@Param("playerIds") List<Integer> playerIds, @Param("week") Integer week);
->>>>>>> 后台统计代码功能
 }
