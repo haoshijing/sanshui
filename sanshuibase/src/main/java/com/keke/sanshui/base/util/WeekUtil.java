b3f36cd6ec21fbe5aec5d47ec9d192a29d8c003b
haoshijing <haoshijing690@163.com>
2017-12-20 23:43:29 +0800

    改变时间处理工具类

--- sanshuibase/src/main/java/com/keke/sanshui/base/util/WeekUtil.java
+++ sanshuibase/src/main/java/com/keke/sanshui/base/util/WeekUtil.java
@@ -1,8 +1,10 @@
 package com.keke.sanshui.base.util;
 
 import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
 
 import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -13,6 +15,8 @@ public final class WeekUtil {
     public static int getCurrentWeek(){
         return getCurrentWeek("");
     }
+
+
     public static int getCurrentWeek(String dateStr){
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR,2017);
@@ -39,16 +43,20 @@ public final class WeekUtil {
     }
 
     public static long getWeekStartTimestamp(int week){
-        Calendar cal = Calendar.getInstance();
-        cal.set(2017,Calendar.OCTOBER,23,0,0,0);
-        cal.set(Calendar.WEEK_OF_YEAR,cal.get(Calendar.WEEK_OF_YEAR)+(week-1));
-        return cal.getTimeInMillis();
+//        Calendar cal = Calendar.getInstance();
+//        cal.set(2017,Calendar.OCTOBER,23,0,0,0);
+//        cal.set(Calendar.WEEK_OF_YEAR,cal.get(Calendar.WEEK_OF_YEAR)+(week-1));
+//        return cal.getTimeInMillis();
+        DateTime dateTime = new DateTime(2017,10,29,0,0,0);
+        return dateTime.plusWeeks(week-1).getMillis();
     }
     public static long getWeekEndTimestamp(int week){
-        Calendar cal = Calendar.getInstance();
-        cal.set(2017,Calendar.OCTOBER,29,23,59,59);
-        cal.set(Calendar.WEEK_OF_YEAR,cal.get(Calendar.WEEK_OF_YEAR)+(week-1));
-        return cal.getTimeInMillis();
+//        Calendar cal = Calendar.getInstance();
+//        cal.set(2017,Calendar.OCTOBER,29,23,59,59);
+//        cal.set(Calendar.WEEK_OF_YEAR,cal.get(Calendar.WEEK_OF_YEAR)+(week-1));
+//        return cal.getTimeInMillis();
+        DateTime dateTime = new DateTime(2017,10,29,23,59,59);
+        return dateTime.plusWeeks(week-1).getMillis();
     }
     public static long getWeekStartTimestamp(){
         return getWeekStartTimestamp(getCurrentWeek());
