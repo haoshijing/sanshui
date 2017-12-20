c99f3dbce8b1a235341d394a3c187d910a55f361
郝仕兢 <haoshijing@corp.netease.com>
2017-12-20 09:18:50 +0800

    修改配置文件

--- sanshuiportal/src/main/java/com/keke/sanshui/pay/alipay/AlipayConfig.java
+++ sanshuiportal/src/main/java/com/keke/sanshui/pay/alipay/AlipayConfig.java
@@ -1,4 +1,5 @@
-﻿package com.keke.sanshui.pay.alipay;
+
+package com.keke.sanshui.pay.alipay;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,36 +9,37 @@ import org.springframework.stereotype.Repository;
 @Data
 public class AlipayConfig {
 
-	@Value("${aliAppId}")
-	private String appId;
+    @Value("${aliAppId}")
+    private String appId;
 
-	@Value("${callbackHost}")
-	private String callbackHost;
+    @Value("${callbackHost}")
+    private String callbackHost;
 
-	@Value("${rsaPrivateKey}")
-	private String rsaPrivateKey;
+    @Value("${rsaPrivateKey}")
+    private String rsaPrivateKey;
 
-	@Value("${rsaPublicKey}")
-	private String rsaPublicKey;
+    @Value("${rsaPublicKey}")
+    private String rsaPublicKey;
 
-	// 请求网关地址
-	public static String URL = "https://openapi.alipay.com/gateway.do";
-	// 编码
-	public static String CHARSET = "UTF-8";
-	// 返回格式
-	public static String FORMAT = "json";
+    // 请求网关地址
+    public static String URL = "https://openapi.alipay.com/gateway.do";
+    // 编码
+    public static String CHARSET = "UTF-8";
+    // 返回格式
+    public static String FORMAT = "json";
 
-	// RSA2
-	public static String SIGNTYPE = "RSA2";
+    // RSA2
+    public static String SIGNTYPE = "RSA2";
 
 
-	public String getNotifyUrl(){
-		String url = new StringBuilder(callbackHost).append("/alipay/callback").toString();
-		return url;
-	}
+    public String getNotifyUrl(){
+        String url = new StringBuilder(callbackHost).append("/alipay/callback").toString();
+        return url;
+    }
 
-	public String getReturnUrl(String orderId){
-		String url = new StringBuilder(callbackHost).append("/alipay/user/").append(orderId).toString();
-		return url;
-	}
+    public String getReturnUrl(String orderId){
+        String url = new StringBuilder(callbackHost).append("/alipay/user/").append(orderId).toString();
+        return url;
+    }
 }
+
